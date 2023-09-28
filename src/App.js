1cbf5f3f87af75803d88870ebfcc8fde44a5f9af
NIhal Patel <47061480+Nihal192@users.noreply.github.com>
2023-09-27 21:31:23 -0400

    App.js

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Todoinput from './Todoinput'
 import TodoList from './TodoList';
 
 function App(){
-  const [listTodo, setListTodo]=useState([]);
-  let addlist =(inputText)=>{
+  const [listTodo, setListTodo]=useState([]);             //This function will store the values that is being written by user.
+  let addlist =(inputText)=>{                    //we have declared the vairable name addlist that will  create a new array that contains all the elements from the existing listTodo array. Then, you add inputText to this new array.
     setListTodo([...listTodo,inputText]);
   }
 
@@ -22,7 +22,7 @@ function App(){
     <div className='main-container'>
       <div className='div-contianer'>
 
-      <Todoinput addlist={addlist}/>
+      <Todoinput addlist={addlist}/>     
       <h1 className='app-heading'>TODO</h1>
       <hr/>
       {listTodo.map((listItem,i)=> {
