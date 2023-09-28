abe77aea293d03de68b05b92ae15ef9cc8fdba60
NIhal Patel <47061480+Nihal192@users.noreply.github.com>
2023-09-27 22:05:37 -0400

    TodoList.js

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,13 +1,13 @@
 import React from 'react'
-
+                                                   //whole idea isto show the items in list manner.
 export default function TodoList(props) {
   return (
     <div>
-      <li className='list-item'>
-        {props.item}
-        <span className='icons'>
-        <i className="fa-solid fa-trash icon-delete" onClick={e=>{
-          props.deleteItem(props.index)
+      <li className='list-item'>  // this is list that store the items
+        {props.item}  //this will send the items to app.js in looping manner
+        <span className='icons'>  //jpg icon of trash that will indicate the 
+        <i className="fa-solid fa-trash icon-delete" onClick={e=>{       
+          props.deleteItem(props.index)  //
         }}></i>
         </span>
       </li>
