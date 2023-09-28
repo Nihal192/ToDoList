811d3a967af4775d29d5aee87be69c180a849213
NIhal Patel <47061480+Nihal192@users.noreply.github.com>
2023-09-27 21:07:52 -0400

    Todoinput.js

--- src/Todoinput.js
+++ src/Todoinput.js
@@ -1,18 +1,18 @@
 import React,{useState} from 'react'
 
 export default function Todoinput(props) {
-  const[inputText,setInputText]= useState('');
+  const[inputText,setInputText]= useState('');   // This fucntion will take input from input text container that is displayed on homepage.
   return (
     <div className='input-contianer'>
       <input type='text' className='input-box-todo'
       placeholder='enter your text'
       value={inputText}
-      onChange={e => {
+      onChange={e => {                         //on change is kind of event listener that invokes setInputText in return which will store the values that are being written in the textbox.
         setInputText(e.target.value)
         }
       }
       />
-      <button className='add-btn' onClick={() => {
+      <button className='add-btn' onClick={() => {     //Button that is being use to add items into the list.
         props.addlist(inputText)
       }}>+</button>
     
